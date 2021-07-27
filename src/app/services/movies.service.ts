fa28886f3dc81e5fc634e425d292c0bdbcd906e7
unknown <camilo.artunduaga@grantitan.com>
2021-07-26 21:31:03 -0500

    prueba para ver si funciona

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -26,7 +26,7 @@ export class MoviesService {
     const hoy = new Date();
     const ultimoDia = new Date(
       hoy.getFullYear(),
-      hoy.getMonth() + 2,
+      hoy.getMonth() + 1,
       0
     ).getDate();
     const mes = hoy.getMonth() + 1;
